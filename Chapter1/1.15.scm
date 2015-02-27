73649afe078b4a056ca4143e1efe4a466ce9faf2
Jeff Hoelter <jeffh@loar.net>
2015-02-27 15:28:56 -0500

    Added exercise 1.15

--- Chapter1/1.15.scm
+++ Chapter1/1.15.scm
@@ -17,7 +17,7 @@
 
 ;; a) There are 5 calls to p for (sine 12.15)
 
-;; b) While this procedure is recursive, it only makes a single call to itself so it is fairly straightforward. Additional calls to p only occur with multiples of 3 (due to the division by 3 before the recusrive call to p) therefore both the space and number of steps is O(log n).
+;; b) While this procedure is recursive, it only makes a single call to itself so it is fairly straightforward. Additional calls to p only occur with multiples of 3 (due to the division by 3 before the recursive call to p) therefore both the space and number of steps is O(log n).
 
 
 

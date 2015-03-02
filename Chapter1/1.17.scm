8f6a1289db27b2f2e2444d0bbcc964715be7964a
Jeff Hoelter <jeffh@loar.net>
2015-03-02 10:59:27 -0500

    Added exercise 1.17

---
+++ Chapter1/1.17.scm
@@ -0,0 +1,1 @@
+(define (double a)          (* a 2))(define (halve a)          (/ a 2))(define (fast-mult a b)  (cond ((= b 0) 0)    ((even? b) (fast-mult (double a) (halve b)))    (else (+ a (fast-mult a (- b 1))))))
